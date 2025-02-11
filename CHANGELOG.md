bb44197ec49a3647c077ed6cb4144c2d49ae4112
Kip Cole <kipcole9@gmail.com>
2025-02-12 09:22:17 +1100

    Update deps and define package

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## Vcard 1.0.0
+
+This is the changelog for Vcard 1.0.0 released on ______.  For older changelogs please consult the release tag on [GitHub](https://github.com/kipcole9/vcard/tags).
+
+### Initial Release
+
+
--- mix.exs
+++ mix.exs
@@ -1,15 +1,23 @@
 defmodule Vcard.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "1.0.0"
 
   def project do
     [
-      app: :vcard,
       version: @version,
-      elixir: "~> 1.6",
+      elixir: "~> 1.14",
+      name: "Vcard",
+      source_url: "https://github.com/kipcole9/vcard",
+      docs: docs(),
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      app: :vcard,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -19,11 +27,53 @@ defmodule Vcard.MixProject do
     ]
   end
 
+  def description do
+    "Vcard parser and serializer"
+  end
+
   defp deps do
     [
-      {:nimble_parsec, git: "https://github.com/plataformatec/nimble_parsec"},
-      {:nimble_csv, "~> 0.4"},
-      {:ex_phone_number, "~> 0.1"}
+      {:nimble_parsec, "~> 1.0"},
+      {:nimble_csv, "~> 1.0"},
+      {:ex_phone_number, "~> 0.4"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Kip Cole"],
+      licenses: ["Apache-2.0"],
+      links: links(),
+      files: [
+        "lib",
+        "config",
+        "mix.exs",
+        "README*",
+        "CHANGELOG*",
+        "LICENSE*"
+      ]
+    ]
+  end
+
+  def links do
+    %{
+      "GitHub" => "https://github.com/kipcole9/vcard",
+      "Readme" => "https://github.com/kipcole9/vcard/blob/v#{@version}/README.md",
+      "Changelog" => "https://github.com/kipcole9/vcard/blob/v#{@version}/CHANGELOG.md"
+    }
+  end
+
+  def docs do
+    [
+      source_ref: "v#{@version}",
+      main: "readme",
+      # logo: "logo.png",
+      extras: [
+        "README.md",
+        "LICENSE.md",
+        "CHANGELOG.md"
+      ],
+      formatters: ["html"]
     ]
   end
 end
